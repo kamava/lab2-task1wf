cef06daf14092ced06860fe5cbb0bce1d60740b2
kamava <75224872+kamava@users.noreply.github.com>
2021-03-04 17:23:41 +0200

    Add files via upload

---
+++ lab2-task4/lab2-task4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace lab2_task4
+{
+
+    public class MyClass<T> where T : new()
+    {
+
+        public static T FacrotyMethod()
+        {
+            return new T();
+        }
+    }
+
+    class Program
+    {/*
+      * Создайте класс MyClass, содержащий статический фабричный метод – T FacrotyMethod(),
+      * который будет порождать экземпляры типа, указанного в качестве параметра типа (указателя места заполнения типом – Т).  
+        */
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+    }
+}
---
+++ lav2-task3/lav2-task3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace lav2_task3
+{
+    
+    public static class Myclass
+    {
+        public static string GetArray(this string list, params string[] strings)
+        {
+            for (int i = 0; i < strings.Length; i++)
+            {
+                list += $"{strings[i]}";
+            }
+            return list;
+        }
+    }
+
+    /*
+     * Создайте расширяющий метод: public static T[] GetArray(this MyList list) 
+     * Примените расширяющий метод к экземпляру типа MyList.
+     * Выведите на экран значения элементов массива, который вернул расширяющий метод GetArray(). 
+    */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("zapolnyte kol masiva");
+            int r = Convert.ToInt32(Console.ReadLine());
+            string[] mass = new string[r];
+            for(int i = 0; i < mass.Length; i++)
+            {
+                Console.WriteLine("vvedite element massiva");
+                mass[i] = Console.ReadLine();
+            }
+            try
+            {
+                string s = "result ".GetArray(mass);
+                Console.WriteLine(s);
+            }
+            catch
+            {
+                Console.WriteLine("vvedite corectnoe znacheniye");
+            }
+           
+
+
+        }
+    }
+}
